b669ff12c2bc7241d0bc4b9e6d5fc453d6369d2a
mabe-chargeo <mabe@chargeo.fr>
2026-07-11 15:50:58 +0200

    Mise à jour API V20

--- src/app/api/webhooks/costructor/route.ts
+++ src/app/api/webhooks/costructor/route.ts
@@ -127,7 +127,8 @@ const costructorPayload = {
   lastName: lastName,
   emails: clientEmail ? [{ email: clientEmail, primary: true }] : undefined,
   phones: formattedPhone ? [{ phone: formattedPhone, primary: true }] : undefined,
-  addresses: addressObj
+  addresses:  addressObj ? [addressObj] : []
+
 };
 
 
